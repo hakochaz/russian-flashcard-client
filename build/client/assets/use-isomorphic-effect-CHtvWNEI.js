ca90b1b04d66b2dfee3dfbba63bd5a75f4c41d2a
Charlie Hackett <charlie209@protonmail.com>
2026-02-05 12:36:44 +0000

    Update build files.

--- build/client/assets/use-isomorphic-effect-CHtvWNEI.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./chunk-EPOLDU6W-Db15p_wQ.js";const o=typeof document<"u"?e.useLayoutEffect:e.useEffect;export{o as u};
---
+++ build/client/assets/use-isomorphic-effect-Chui70N-.js
@@ -0,0 +1,1 @@
+import{b as e}from"./index-CWD__XTI.js";const o=typeof document<"u"?e.useLayoutEffect:e.useEffect;export{o as u};
